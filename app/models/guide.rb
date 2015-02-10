247a539ff2a970bd45885bd7d7d873382806c4cd
David Sin <davidsin1990@gmail.com>
2015-02-09 20:15:15 -0500

    took out votable options for now, wasnt working

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -2,4 +2,10 @@ class Guide < ActiveRecord::Base
   belongs_to :champion
   belongs_to :user
   has_many :comments, as: :commentable
+
+  # acts_as_votable
+
+  # def score
+  #   self.get_upvotes.count - self.get_downvotes.count
+  # end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,10 @@
 class Topic < ActiveRecord::Base
   belongs_to :user
   has_many :comments, as: :commentable
+
+  # acts_as_votable
+
+  # def score
+  #   self.get_upvotes.count - self.get_downvotes.count
+  # end
 end
